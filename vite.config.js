86f4ab6d9806d6a23a7fe8927ec35807a0a7a52e
errolbrodie-sudo <errolbrodie@gmail.com>
2026-09-01 11:30:32 +0300

    fix: add all html pages to vite config

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        affiliate: resolve(__dirname, 'affiliate-disclosure.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        disclaimer: resolve(__dirname, 'disclaimer.html'),
+        education: resolve(__dirname, 'education-report.html'),
+        insurance: resolve(__dirname, 'insurance-report.html'),
+        pet: resolve(__dirname, 'pet-report.html'),
+        embassy: resolve(__dirname, 'portuguese-embassy-consulates-usa.html'),
+        platforms: resolve(__dirname, 'relocation-platforms-competitive-dossier.html'),
+        rent: resolve(__dirname, 'rent-report.html'),
+        terms: resolve(__dirname, 'terms.html'),
+        visa: resolve(__dirname, 'visa-report.html'),
+      },
+    },
+  },
+});
